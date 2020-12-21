dc3b08eb7f0f060a96f94087516ef1f919314984
Matt Williams <mattinzhuhai@outlook.com>
2020-12-20 19:30:03 -0800

    :hammer:  Renaming

---
+++ vimdiesel/diesellib/configmanager.py
@@ -0,0 +1,8 @@
+"""
+Manage any and all configuration files for vim-diesel
+"""
+
+
+class ConfigurationManager:
+    """
+    """
---
+++ vimdiesel/dieselutils/settings.py
@@ -0,0 +1,10 @@
+"""
+Handle all constants and any setting vim-diesel might need
+"""
+
+
+def test():
+    """
+    test func
+    """
+    print(f'Testing from: {__file__}')
